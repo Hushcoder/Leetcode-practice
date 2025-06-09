172de7820fdbd97678f02fbd25e69d0e5ce574b0
Vishal <keshavchauhan169@gmail.com>
2025-06-10 00:51:34 +0530

    Time: 2 ms (66.86%) | Memory: 18.9 MB (18.40%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,9 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        a = 0
-        for i in range(len(nums)+1):
-            if i not in nums:
-                a = i
-            else:
-                pass
-        return a
+        
+        # My code complexity :- O[n^2]
+        # a = 0
+        # for i in range(len(nums)+1):
+        #     if i not in nums:
+        #         a = i
+        #     else:
+        #         pass
+        # return a
+
+        # Code :- O[n]
+        n = len(nums)
+        total_sum = n*(n+1)//2
+        actual_sum = sum(nums)
+        return total_sum-actual_sum
+
