7918e011f61b2ba0df672d802102b2638e670481
Vishal <keshavchauhan169@gmail.com>
2025-06-15 02:24:31 +0530

    Time: 1 ms (64.18%) | Memory: 41.4 MB (77.23%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -7,13 +7,14 @@ class Solution {
         String last = strs[strs.length-1];
 
         int min = first.length() < last.length() ? first.length() : last.length();
-        int i;
-        for(i=0; i< min ;i++)
+        int j = 0;
+        for(int i=0; i< min ;i++)
         {
             if(first.charAt(i) != last.charAt(i))
             break;
+            j++;
         }
 
-        return strs[0].substring(0,i);
+        return strs[0].substring(0,j);
     }
 }
