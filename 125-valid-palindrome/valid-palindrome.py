4f18e151ffcbbee38209bb952a7f91c7f6678a3e
Vishal <keshavchauhan169@gmail.com>
2025-06-20 16:21:44 +0530

    Time: 7 ms (81.39%) | Memory: 19.2 MB (27.79%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,18 +2,27 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         import re
         new_str = re.sub(r'[^a-zA-Z0-9]', '', s)
-        new_str_lower = ''
-        for char in new_str:
-            if char.isupper():
-                new_str_lower += char.lower()
-            else:
-                new_str_lower += char
+        # Complexity :- O(N) , Space :- O(N)
+        # new_str_lower = ''
+        # for char in new_str:
+        #     if char.isupper():
+        #         new_str_lower += char.lower()
+        #     else:
+        #         new_str_lower += char
 
-        rev_str = ''
-        for i in range(len(new_str_lower)-1,-1,-1):
-            rev_str += new_str_lower[i]
+        # rev_str = ''
+        # for i in range(len(new_str_lower)-1,-1,-1):
+        #     rev_str += new_str_lower[i]
         
-        return True if rev_str == new_str_lower else False
+        # return True if rev_str == new_str_lower else False
+
+        # Above code's shorter form
+        comp_str = new_str.lower()
+
+        if comp_str[::-1] == comp_str:
+            return True
+        else:
+            return False
 
 
 
