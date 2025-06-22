2fee541c579c9f60aec79d414750bfb570defb89
Vishal <keshavchauhan169@gmail.com>
2025-06-22 17:03:58 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.89%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.py
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.py
@@ -1,31 +1,31 @@
 class Solution:
     def divideString(self, s: str, k: int, fill: str) -> List[str]:
-        # n = len(s)
-        # rest_str = ""
-        # if n % k == 0:
-        #     res = [s[i:i+k] for i in range(0, n , k)]
-        #     return res
-        # else:
-        #     next_mul = ((n + k - 1) // k) * k
-        #     # for just_great in k_mul:
-        #     #     if just_great > n:
-        #     #         sol_mul.append(just_great)
-        #     diff = next_mul - n
-        #     while diff > 0:
-        #         rest_str += fill
-        #         diff -= 1
-        #     # Or Instead if while use s += fill * diff
-        #     final_str = s + rest_str
-        #     res = [final_str[i:i+k] for i in range(0, len(final_str) , k)]
+        n = len(s)
+        rest_str = ""
+        if n % k == 0:
+            res = [s[i:i+k] for i in range(0, n , k)]
+            return res
+        else:
+            next_mul = ((n + k - 1) // k) * k
+            # for just_great in k_mul:
+            #     if just_great > n:
+            #         sol_mul.append(just_great)
+            diff = next_mul - n
+            while diff > 0:
+                rest_str += fill
+                diff -= 1
+            # Or Instead if while use s += fill * diff
+            final_str = s + rest_str
+            res = [final_str[i:i+k] for i in range(0, len(final_str) , k)]
             
-        #     return res
+            return res
 
 
-            # Optimized Implementation 
-            n = len(s)
-            diff = (k - (n%k))%k # handles the n % k == 0
-            s += fill * diff
+            # # Optimized Implementation 
+            # n = len(s)
+            # diff = (k - (n%k))%k # handles the n % k == 0
+            # s += fill * diff
 
-            return [s[i:i+k] for i in range(0, len(s) , k)]
+            # return [s[i:i+k] for i in range(0, len(s) , k)]
             
         
