9e40a973ea6646ba0ccc830ecddf314d773281f2
Vishal <keshavchauhan169@gmail.com>
2025-06-26 14:34:42 +0530

    Time: 7 ms (33.78%) | Memory: 20.2 MB (7.16%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,29 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-
-        # My Code :- TC=> O[NlogN], SC=> O[N]
-        # n = len(nums)
-        # if n <= 1:
-        #     return nums[0]
-        # freq = [0] * n
-        # l = sorted(nums)
-        # for i in range(1,n):
-        #     if l[i-1] == l[i]:
-        #         freq[i-1] = 1
-        #         freq[i] = 1
-        #     else:
-        #         pass
-
-        # for i in range(n):
-        #     if freq[i] == 0:
-        #         return l[i] 
-
-        # Optimized Code :- TC=> O[N], SC=> O[1]
-        n = len(nums)
-        if n<=1:
-            return nums[0]
-        num = 0
-        for i in range(n):
-            num ^= nums[i]
-        return num
-
+        freq_map = set()
+        for i in range(len(nums)):
+            if nums[i] not in freq_map:
+                freq_map.add(nums[i])
+            else:
+                freq_map.remove(nums[i])
+        
+        return list(freq_map)[0]
