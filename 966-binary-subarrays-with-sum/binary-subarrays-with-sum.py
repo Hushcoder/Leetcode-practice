2700d70673015aaebb38fbcb6ead74a81cc090fe
Vishal <keshavchauhan169@gmail.com>
2025-07-01 22:45:50 +0530

    Time: 28 ms (72.31%) | Memory: 18.8 MB (60.69%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -0,0 +1,38 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        n = len(nums)
+        if sum(nums) == 0 and not goal:
+            return (n*(n+1)//2)
+        
+        l, r, summa, c =  0, 0, 0, 0
+
+        while(r < n):
+            summa += nums[r]
+            
+            while(summa > goal):
+                summa -= nums[l]
+                l += 1
+            
+            c += (r-l+1)
+            r += 1
+        
+        return c - self.SubarraysWithSum(nums, goal - 1)
+   
+    def SubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        k = len(nums)
+        if goal < 0 :
+            return 0
+
+        l, r, summ, count =  0, 0, 0, 0
+
+        while(r < k):
+            summ += nums[r]
+            
+            while(summ > goal):
+                summ -= nums[l]
+                l += 1
+            
+            count += (r-l+1)
+            r += 1
+        
+        return count
