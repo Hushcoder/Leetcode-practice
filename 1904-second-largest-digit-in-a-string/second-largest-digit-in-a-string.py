1d8e54f2e05c11ecb3a37156be430622be7ae7d9
Vishal <keshavchauhan169@gmail.com>
2025-07-10 17:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.62%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
@@ -2,7 +2,7 @@ class Solution:
     def secondHighest(self, s: str) -> int:
         max_1 = -1
         max_2 = -1
-
+        # My first Approach : 
         # for i in range(len(s)):
         #     if s[i] not in [range(10)]:
         #         pass
@@ -13,13 +13,31 @@ class Solution:
         #         # print(f'{max_1} : {max_2}')
         
         # return max_2    # always return -1 say "54321" , here it never gets updated.
+
+        # Corrected :===> let's say 5 3 4 , then check if num > max_2 if true then put max_2 = num ---> hop till reach 4.
+
         setv = set()
         for n in s:
             if n.isdigit():
                 setv.add(int(n))
-        sorted_ls = list(sorted(setv, reverse=True)) # for descending order
-
-        if len(setv) < 2:
-            return -1
         
-        return sorted_ls[1]
+        for num in setv:
+            if num > max_1:
+                max_2 = max_1
+                max_1 = num
+            elif num > max_2:
+                max_2 = num
+
+        return max_2
+
+        # Method :====> 2
+        # setv = set()
+        # for n in s:
+        #     if n.isdigit():
+        #         setv.add(int(n))
+        # # sorted_ls = list(sorted(setv, reverse=True)) # for descending order
+
+        # # if len(setv) < 2:
+        # #     return -1
+        
+        # # return sorted_ls[1]
