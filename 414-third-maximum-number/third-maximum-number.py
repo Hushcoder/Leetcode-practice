75e3000e98314add772333693ea1b455d323277c
Vishal <keshavchauhan169@gmail.com>
2025-08-01 23:01:11 +0530

    Time: 1 ms (42.57%) | Memory: 18.3 MB (89.50%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -1,30 +1,31 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         
-        nums = sorted(set(nums), reverse=True) # O(nlogn)
+        # nums = sorted(set(nums), reverse=True) # O(nlogn)
         
-        return nums[2] if len(nums) >= 3 else nums[0]
+        # return nums[2] if len(nums) >= 3 else nums[0]
 
         ## In => O(n) TC
 
-        # mx1 = mx2 = mx3 = float('-inf')
+        mx1 = mx2 = mx3 = float('-inf')
 
-        # for num in nums:
-        #     if num == mx1 or num == mx2 or num == mx3:
-        #         continue
-        #     elif num > mx1:
-        #         mx3 = mx2
-        #         mx2 = mx1
-        #         mx1 = num
-        #     elif num > mx2 and num < mx1:
-        #         mx3 = mx2
-        #         mx2 = num
-        #     else:
-        #         mx3 = num
-        # if mx3 == float('-inf'):
-        #     return mx1
-        # else:
-        #     return mx3
+        for num in nums:
+            if num == mx1 or num == mx2 or num == mx3:
+                continue
+            elif num > mx1:
+                mx3 = mx2
+                mx2 = mx1
+                mx1 = num
+            elif num > mx2:
+                mx3 = mx2
+                mx2 = num
+            elif num > mx3:
+                mx3 = num
+        
+        if mx3 == float('-inf'):
+            return mx1
+        else:
+            return mx3
 
 
  
