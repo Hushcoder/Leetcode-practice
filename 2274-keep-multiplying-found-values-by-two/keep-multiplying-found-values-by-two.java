548232b7830c697c39fefc72c18fa34e42930594
Vishal <keshavchauhan169@gmail.com>
2025-11-19 19:52:57 +0530

    Time: 5 ms (16.63%) | Memory: 46.3 MB (12.30%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -3,11 +3,22 @@ class Solution {
         int n = nums.length;
         int val = original;
 
-        for(int i=0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(nums[j] == val){
-                val = 2*val;
-                }
+        // for(int i=0; i<n; i++){
+        //     for(int j = 0; j<n; j++){
+        //         if(nums[j] == val){
+        //         val = 2*val;
+        //         }
+        //     }
+        // }
+        // return val;
+        HashMap<Integer , Integer> map = new HashMap<>();
+        for(int it : nums){
+            map.put(it , 1);
+        }
+
+        for(int i =0; i<n; i++){
+            if(map.containsKey(val)){
+                val = val*2;
             }
         }
         return val;
